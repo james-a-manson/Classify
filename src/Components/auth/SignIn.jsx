3ecf1b36394e120f3a95a43e3f4e2a404b75bbd7
James Manson <s4097015@student.rmit.edu.au>
2024-09-20 23:19:59 +1000

    Added posting to firebase for email and password

--- src/Components/auth/SignIn.jsx
+++ src/Components/auth/SignIn.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        console.log(userCredential);
+    }).catch((error) => {
+        console.log(error);
+    });
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={signIn}>
         <h1>Log In</h1>
         <input
           type="email"
@@ -20,6 +32,7 @@ const SignIn = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         ></input>
+        <button type="submit">Log In</button>
       </form>
     </>
   );
