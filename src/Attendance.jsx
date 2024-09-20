6b43c3e2658b38be851f77b8455dadc044d66510
MarvenW <s4091504@student.rmit.edu.au>
2024-09-21 00:11:06 +1000

    Attendance.jSEx and Attendance.css files added - basic framework for the attendance component

---
+++ src/Attendance.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+import "./Attendance.css";
+
+
+export default function Attendance(props) {
+  return (
+    <h1>ATtendance header</h1>
+  );
+};
+
