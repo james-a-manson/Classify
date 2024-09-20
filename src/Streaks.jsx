0c3c4bdd58af0cde8d840dc18525fe82ee30a86e
MarvenW <s4091504@student.rmit.edu.au>
2024-09-20 23:27:31 +1000

    Added baseline Streaks component framework

---
+++ src/Streaks.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./components/Header";
+import "./Streaks.css";
+
+export default function Streaks() {
+  return (
+    <h1>temporary header</h1>
+  );
+};
