00b90d4fb5bb22030e9def74e11f9ff848952ecf
TJM1009 <trenton.j.ma@gmail.com>
2024-09-20 20:49:08 +1000

    Added import of leaderboard on App.jsx and added placeholder text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg' //delete these two later, not needed anymore
 import viteLogo from '/vite.svg'           //also delete the logo files themselves
 import Header from './components/Header'
+import Leaderboard from './Leaderboard'
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +13,7 @@ function App() {
   return (
     <>
       <Header title = 'hello world'/>
-      
+      <Leaderboard />
     </>
   )
 }
--- src/Leaderboard.jsx
+++ src/Leaderboard.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import Header from "./components/Header";
 import './index.css';
 
 function Leaderboard() {
+    return (
+        <div>
+        <Header title = 'Leaderboard'/>
+        <ul>
+            <li>Example list</li>
+            <li>Blah</li>
+            <li>Blah</li>
 
+
+        </ul>
+        </div>
+    )
 }
 
 export default Leaderboard;
