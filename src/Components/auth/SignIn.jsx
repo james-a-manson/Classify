f82a3ecd024df3e5c76fdf206ef5f02a0ffab61c
James Manson <s4097015@student.rmit.edu.au>
2024-09-20 23:09:19 +1000

    Added state functionality to sign in

--- src/Components/auth/SignIn.jsx
+++ src/Components/auth/SignIn.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignIn = () => {
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');   
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <div>
-        <form>
-            <h1>Log In</h1>
-            <input type="email" placeholder='Enter your email' value={email}></input>
-            <input type="password" placeholder='Enter your password' value={password}></input>
-        </form>
-    </div>
-  )
-}
+    <>
+      <form>
+        <h1>Log In</h1>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </form>
+    </>
+  );
+};
 
-export default SignIn
+export default SignIn;
