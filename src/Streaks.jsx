bd441d6750bab53783c628bfaf63c7db7128df4b
MarvenW <s4091504@student.rmit.edu.au>
2024-09-20 23:54:27 +1000

    Updated Streaks page - Now needs updated CSS + link to database's streak value.

--- src/Streaks.jsx
+++ src/Streaks.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import "./Streaks.css";
+import firePNG from "./assets/fire.png";
+
 
 export default function Streaks(props) {
   return (
     <div>
-      <h1>Your streak count:</h1>
-      <h1>{props.streak}</h1>
+      <h1 id="Phrase">Your streak count:</h1>
+      <h1 id="Number">{props.streak}</h1>
+      <img src={firePNG} alt=":fire emoji:"></img>
     </div>
   );
 };
