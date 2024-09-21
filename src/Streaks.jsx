2eaa843984006a1605a37e4b5392a8595b73f8ff
James Manson <s4097015@student.rmit.edu.au>
2024-09-21 15:08:52 +1000

    Implemented pulling streak data from database

--- src/Streaks.jsx
+++ src/Streaks.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import "./Streaks.css";
 import firePNG from "./assets/fire.png";
@@ -6,22 +6,45 @@ import firePNG from "./assets/fire.png";
 import { db } from "./firebase";
 import { query, getDoc, collection, where, onSnapshot, snapshotEqual } from "firebase/firestore";
 import { auth } from "./firebase";
-
-const currentUser = auth.currentUser;
-console.log(currentUser);
-const colRef = collection(db, 'students');
-const q = query(colRef, where('student_email', '==', currentUser));
-
-onSnapshot(q, (snapshot) => {
-  let currentUserData = []
-  snapshot.docs.forEach((doc) => {
-    currentUserData.push({ ...doc.data(), id: doc.id})
-  })
-  console.log(currentUserData)
-})
-
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Streaks(props) {
+  const [currentUserData, setCurrentUserData] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribeAuth();
+    };
+
+  }, []);
+
+
+useEffect(() => {
+  if (currentUser) {
+    const colRef = collection(db, 'students');
+    const q = query(colRef, where('student_email', '==', currentUser.email)); 
+
+    const unsubscribeSnapshot = onSnapshot(q, (snapshot) => {
+      const currentUserData = [];
+      snapshot.docs.forEach((doc) => {
+        currentUserData.push({ ...doc.data(), id: doc.id });
+      });
+      setCurrentUserData(currentUserData); 
+    });
+
+    return () => unsubscribeSnapshot();
+  }
+}, [currentUser]);
+
   return (
     <div>
       <h1 id="Phrase">Your streak count:</h1>
@@ -29,7 +52,7 @@ export default function Streaks(props) {
       <img src={firePNG} alt=":fire emoji:"></img>
     </div>
   );
-};
+}
 
 Streaks.defaultProps = {
   streak: "Streak value not found"
