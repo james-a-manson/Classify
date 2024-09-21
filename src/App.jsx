3cca1d641b1456a7bf292d500c935f9ab8200369
James Manson <s4097015@student.rmit.edu.au>
2024-09-21 14:37:16 +1000

    Implemented auth on App.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 import reactLogo from "./assets/react.svg"; //delete these two later, not needed anymore
 import viteLogo from "/vite.svg"; //also delete the logo files themselves
 import Header from "./components/Header";
@@ -11,10 +13,28 @@ import Attendance from "./Attendance";
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => {
+      listen();
+    };
+  });
 
   return (
     <>
+      {authUser ? (
+        <h1>Working</h1>
+      ) : (
+        <AuthDetails />
+      )}
         <AuthDetails/>
     </>
   );
